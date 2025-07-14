bf5d6589b7ef6029f68b4cc59ac8d4a9b40cf035
Manos Mastronikolas <csd5450@csd.uoc.gr>
2025-07-14 13:19:28 +0300

    Need to make init

--- charmod.c
+++ charmod.c
@@ -3,33 +3,46 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 
-static int majorNum;
+int mod_open(struct inode* inode, struct file *fp);
+int mod_release(struct inode *inode, struct file* fp);
+ssize_t mod_read(struct file* fp, char __user *buff, size_t count, loff_t *offp);
+ssize_t mod_write(struct file* fp, char __user *buff, size_t count, loff_t *offp);
+int mod_init(struct cdev* cdev, struct file_operations* fops);
+
+int majorNum = 0;
+
+struct mod_dev{
+
+    
+};
 
 struct file_operations fops = {
-        .open = *mod_open,
-        .release = NULL,
-        .read = NULL,
-        .write = NULL
+  .owner = THIS_MODULE,
+  .open = mod_open,
+  .release = mod_release,
+  .read = mod_read,
+  .write = mod_write
 
 };
+
+int mod_init(struct cdev* cdev, struct file_operations* fops){
+
+
+
+}
+
 
 int mod_open(struct inode* inode, struct file *fp){
     printk("Opened module!\n");
     return 0;
 }
 
-int mod_release(){
+int mod_release(struct inode *inode, struct file* fp){
   printk("Closing module!\n");
   return 0;
 
 }
 
-int mod_read(){
-
-}
-
-int mod_write(){
-}
 
 
 
